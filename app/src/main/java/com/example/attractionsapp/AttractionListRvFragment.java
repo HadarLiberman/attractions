9ae35093ed56e822031fb416899ba2ad665066d5
HadarLiberman <libermanhadar@gmail.com>
2022-02-09 20:29:47 +0200

    check

--- app/src/main/java/com/example/attractionsapp/AttractionListRvFragment.java
+++ app/src/main/java/com/example/attractionsapp/AttractionListRvFragment.java
@@ -27,6 +27,8 @@ public class AttractionListRvFragment extends Fragment {
     @Override
 
 
+
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_user_attractions_list,container,false);
